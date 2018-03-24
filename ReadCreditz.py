96cc813747fb5ba81117d89be9c29599e1081ec0
ChrisMiuchiz <chris000005@gmail.com>
2018-03-23 21:44:27 -0400

    Add files via upload

---
+++ ReadCreditz.py
@@ -0,0 +1,14 @@
+from HandheldCore import Handheld
+import struct
+
+def UndoHCD(b):
+    return sum( [int(hex(e)[2:]) * 100**i for i, e in enumerate(b)])
+
+def main():
+    handheld = Handheld.FindHandhelds()[0]
+    data = handheld.ReadPage(1, 0xFF)
+    creditz = UndoHCD(data[0x9AA : 0x9AE])
+    print(creditz)
+
+if __name__ == '__main__':
+    main()
